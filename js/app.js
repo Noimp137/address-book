be1fd4bc0c3d2420070e25faf7a7a6f0c1920baa
Kevin-X-fyx <75200021+Kevin-X-fyx@users.noreply.github.com>
2024-11-25 00:50:55 +0800

    Delete js directory

--- js/app.js
+++
@@ -1,8 +0,0 @@
-import { initLoginPage } from './login.js';
-import { initContactPage } from './contacts.js';
-import './events.js';
-
-document.addEventListener('DOMContentLoaded', () => {
-  initLoginPage();
-  initContactPage();
-});
